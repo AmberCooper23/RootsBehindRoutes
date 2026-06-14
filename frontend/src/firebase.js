627fabcdcbf743e650a8cddeb775585cf5d02c1f
AmberCooper23 <2677396@students.wits.ac.za>
2026-06-14 12:19:05 +0200

    firebase config

---
+++ frontend/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAUg96hbTN44vrI3XXFtZckOk4mhPAZilY",
+  authDomain: "rootsbehindroutes.firebaseapp.com",
+  projectId: "rootsbehindroutes",
+  storageBucket: "rootsbehindroutes.firebasestorage.app",
+  messagingSenderId: "606547648299",
+  appId: "1:606547648299:web:7d018c9386771b5a772f6e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
