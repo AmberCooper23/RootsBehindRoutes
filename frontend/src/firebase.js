ea84497d22c9e3e2098ec2089ee469833f55a9a1
AmberCooper23 <2677396@students.wits.ac.za>
2026-06-14 12:54:36 +0200

    setup firebase again lol

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAUg96hbTN44vrI3XXFtZckOk4mhPAZilY",
   authDomain: "rootsbehindroutes.firebaseapp.com",
   projectId: "rootsbehindroutes",
-  storageBucket: "rootsbehindroutes.firebasestorage.app",
+  storageBucket: "rootsbehindroutes.appspot.com",
   messagingSenderId: "606547648299",
   appId: "1:606547648299:web:7d018c9386771b5a772f6e",
 };
