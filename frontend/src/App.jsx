002cee786f8249241a05f870a4fc0db62ac027c7
AmberCooper23 <2677396@students.wits.ac.za>
2026-06-24 21:06:05 +0200

    fixing index styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { NavBar } from "./components/NavBar/NavBar";
 import { LandingPage } from "./pages/LandingPage/LandingPage";
