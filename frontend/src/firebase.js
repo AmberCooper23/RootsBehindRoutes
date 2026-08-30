0514cb6ad0731365ef78e1ab8200b2a16363c4e7
AmberCooper23 <2677396@students.wits.ac.za>
2026-08-31 00:52:34 +0200

    cgfhndrhfc

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,6 +3,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { firebaseConfig } from "../firebaseConfig";
 
 const app = initializeApp(firebaseConfig);
+console.log("Firebase config at runtime:", firebaseConfig);
 
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
