3e385f503063451e941b6a5294daf5c1891ea5fe
AmberCooper23 <2677396@students.wits.ac.za>
2026-08-31 11:05:40 +0200

    env var fix

--- server/firebaseConfig.js
+++ server/firebaseConfig.js
@@ -1,13 +1,15 @@
 const admin = require("firebase-admin");
+const path = require("path");
 
-// Only initialize once (Render restarts can re-run this file)
 if (!admin.apps.length) {
+  const serviceAccount = require(
+    path.join(__dirname, "serviceAccountKey.json"),
+  );
+
   admin.initializeApp({
-    credential: admin.credential.cert(
-      JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT),
-    ),
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    credential: admin.credential.cert(serviceAccount),
+    projectId: serviceAccount.project_id,
+    storageBucket: `${serviceAccount.project_id}.appspot.com`,
   });
 }
 
